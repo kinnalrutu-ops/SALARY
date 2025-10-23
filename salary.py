13601e1220b5901bbfb330de00e465eee2c687a3
kinnalrutu-ops <kinnalrutu@gmail.com>
2025-10-23 15:07:20 +0530

    Add salary bonus calculator program

---
+++ salary.py
@@ -0,0 +1,9 @@
+# Salary Bonus Calculator Program - Bonus Added
+
+salary = float(input("Enter employee's salary: "))
+
+bonus = salary * 0.10
+total_salary = salary + bonus
+
+print("Original Salary:", salary)
+print("Bonus Added:", total_salary)

780bf41186465f05ec4719bf1c2cfef7d616e666
kinnalrutu-ops <kinnalrutu@gmail.com>
2025-10-23 15:10:20 +0530

    save

--- salary.py
+++ salary.py
@@ -1,9 +1,14 @@
-# Salary Bonus Calculator Program - Bonus Added
+
 
 salary = float(input("Enter employee's salary: "))
 
+
 bonus = salary * 0.10
-total_salary = salary + bonus
+total_with_bonus = salary + bonus
+
+tax = total_with_bonus * 0.05
+final_salary = total_with_bonus - tax
 
 print("Original Salary:", salary)
-print("Bonus Added:", total_salary)
+print("Bonus Added:", total_with_bonus)
+print("After Tax Deduction:", final_salary)
